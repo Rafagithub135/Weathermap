7e19679bfa5680796d56306dcca3c5f4ecc064cf
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2022-12-02 17:05:24 -0500

    modified code

--- js/main-js.js
+++ js/main-js.js
@@ -1,3 +1,109 @@
+const app = {
+    init: () => {
+        document
+            .getElementById('btnGet')
+            .addEventListener('click', app.fetchWeather);
+        document
+            .getElementById('btnCurrent')
+            .addEventListener('click', app.getLocation);
+    },
+    fetchWeather: (ev) => {
+        //use the values from latitude and longitude to fetch the weather
+        let lat = document.getElementById('latitude').value;
+        let lon = document.getElementById('longitude').value;
+        let key = '06cc7efd0e5386068ec3c390bcfd0183';
+        let lang = 'en';
+        let units = 'metric';
+        let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+        //fetch the weather
+        fetch(url)
+            .then((resp) => {
+                if (!resp.ok) throw new Error(resp.statusText);
+                return resp.json();
+            })
+            .then((data) => {
+                app.showWeather(data);
+            })
+            .catch(console.err);
+    },
+    getLocation: (ev) => {
+        let opts = {
+            enableHighAccuracy: true,
+            timeout: 1000 * 10, //10 seconds
+            maximumAge: 1000 * 60 * 5, //5 minutes
+        };
+        navigator.geolocation.getCurrentPosition(app.ftw, app.wtf, opts);
+    },
+    ftw: (position) => {
+        //got position
+        document.getElementById('latitude').value =
+            position.coords.latitude.toFixed(2);
+        document.getElementById('longitude').value =
+            position.coords.longitude.toFixed(2);
+    },
+    wtf: (err) => {
+        //geolocation failed
+        console.error(err);
+    },
+    showWeather: (resp) => {
+        console.log(resp);
+        let row = document.querySelector('.weather.row');
+        //clear out the old weather and add the new
+        // row.innerHTML = '';
+        row.innerHTML = resp.daily
+            .map((day, idx) => {
+                if (idx <= 2) {
+                    let dt = new Date(day.dt * 1000); //timestamp * 1000
+                    let sr = new Date(day.sunrise * 1000).toTimeString();
+                    let ss = new Date(day.sunset * 1000).toTimeString();
+                    return `<div class="col">
+              <div class="card">
+              <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                <img
+                  src="http://openweathermap.org/img/wn/${
+                        day.weather[0].icon
+                    }@4x.png"
+                  class="card-img-top"
+                  alt="${day.weather[0].description}"
+                />
+                <div class="card-body">
+                  <h3 class="card-title">${day.weather[0].main}</h3>
+                  <p class="card-text">High ${day.temp.max}&deg;C Low ${
+                        day.temp.min
+                    }&deg;C</p>
+                  <p class="card-text">High Feels like ${
+                        day.feels_like.day
+                    }&deg;C</p>
+                  <p class="card-text">Pressure ${day.pressure}mb</p>
+                  <p class="card-text">Humidity ${day.humidity}%</p>
+                  <p class="card-text">UV Index ${day.uvi}</p>
+                  <p class="card-text">Precipitation ${day.pop * 100}%</p>
+                  <p class="card-text">Dewpoint ${day.dew_point}</p>
+                  <p class="card-text">Wind ${day.wind_speed}m/s, ${
+                        day.wind_deg
+                    }&deg;</p>
+                  <p class="card-text">Sunrise ${sr}</p>
+                  <p class="card-text">Sunset ${ss}</p>
+                </div>
+              </div>
+            </div>
+          </div>`;
+                }
+            })
+            .join(' ');
+    },
+};
+
+app.init();
+
+
+
+
+
+
+
+
+
 const weatherURL = "http://api.openweathermap.org/data/2.5/weather";
 const fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast";
 const oneCallURL = "https://api.openweathermap.org/data/2.5/onecall";
@@ -12,7 +118,51 @@ let currCloud = $("#currentClouds");
 let hoyRise = $("#sunrise");
 let hoySet = $("#sunset");
 
+$.get(fiveDayURL, {
+    APPID: WEATHERMAP_API,
+    q: "Philadelphia, US",
+    units: "imperial"
+}).done(function (data) {
+    // for (let i = 0; i < data.list.length; i++) {
+    //     if (i < 5){
+    //     console.log(dailyForecast.temp.max);
+    //     console.log(dailyForecast.temp.min);
+    //     let date = new Date(data.list[i].dt * 1000);
+    //     let day = date.getDay();
+    //     let hour = date.getHours();
+    //     if (hour === 12) {
+    //         let temp = data.list[i].main.temp;
+    //         let icon = data.list[i].weather[0].icon;
+    //         let desc = data.list[i].weather[0].description;
+    //         let wet = data.list[i].main.humidity;
+    //         let vis = data.list[i].visibility;
+    //         let cloud = data.list[i].clouds.all;
+    //         let rise = data.list[i].sys.sunrise;
+    //         let set = data.list[i].sys.sunset;
 
+
+
+});
+$.get(weatherURL, {
+    APPID: WEATHERMAP_API,
+    q: "Philadelphia, US",
+    units: "imperial"
+}).done(function (data) {
+});
+$.get(fiveDayURL, {
+    APPID: WEATHERMAP_API,
+    q: "Philadelphia, US",
+    units: "imperial"
+}).done(function (data) {
+    console.log('5 day forecast', data);
+});
+mapboxgl.accessToken = MAPBOX_API;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 15,
+    center: [-75.2008, 39.9390]
+});
 $.get(oneCallURL, {
     APPID: WEATHERMAP_API,
     lat:    39.9390,
